0a5f72410116f5fcc24d3485fc6ceed407c69712
Joseph Callen <jcallen@redhat.com>
2026-08-31 15:53:57 -0400

    feat: wire up all tools and lifecycle hooks in main extension entrypoint

---
+++ extensions/index.ts
@@ -0,0 +1,14 @@
+import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+import registerProw from "./prow/index.js";
+import registerPrTools from "./pr/index.js";
+import registerCiTools from "./ci/index.js";
+import registerJiraTools from "./jira/index.js";
+import { registerPrecommitHook } from "./precommit/hook.js";
+
+export default function (pi: ExtensionAPI): void {
+  registerProw(pi);
+  registerPrTools(pi);
+  registerCiTools(pi);
+  registerJiraTools(pi);
+  registerPrecommitHook(pi);
+}
